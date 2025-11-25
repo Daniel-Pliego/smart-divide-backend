f5a6288a07ce234979ba287e90c9fa85bd7065e2
Daniel-Pliego <a20216405@alumnos.uady.mx>
2025-11-24 21:29:57 -0600

    feat: enhance payment event handling with balance validation and update event structure

--- src/main/java/mr/limpios/smart_divide_backend/aplication/services/PaymentService.java
+++ src/main/java/mr/limpios/smart_divide_backend/aplication/services/PaymentService.java
@@ -1,11 +1,14 @@
 package mr.limpios.smart_divide_backend.aplication.services;
 
 import static mr.limpios.smart_divide_backend.domain.constants.ExceptionsConstants.GROUP_NOT_FOUND;
+import static mr.limpios.smart_divide_backend.domain.constants.ExceptionsConstants.NO_EXISTING_DEBTS_FOR_USER_PAIR;
+import static mr.limpios.smart_divide_backend.domain.constants.ExceptionsConstants.PAYMENT_AMOUNT_EXCEEDS_DEBT;
 import static mr.limpios.smart_divide_backend.domain.constants.ExceptionsConstants.USER_NOT_FOUND;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.context.ApplicationEventPublisher;
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import mr.limpios.smart_divide_backend.aplication.repositories.ExpenseGroupBalanceRepository;
 import mr.limpios.smart_divide_backend.aplication.repositories.GroupRepository;
 import mr.limpios.smart_divide_backend.aplication.repositories.PaymentRepository;
 import mr.limpios.smart_divide_backend.aplication.repositories.UserRepository;
@@ -20,7 +24,9 @@ import mr.limpios.smart_divide_backend.domain.dto.CreatePaymentDTO;
 import mr.limpios.smart_divide_backend.domain.dto.PaymentDetailDTO;
 import mr.limpios.smart_divide_backend.domain.dto.PaymentUserDTO;
 import mr.limpios.smart_divide_backend.domain.events.PaymentCreatedEvent;
+import mr.limpios.smart_divide_backend.domain.exceptions.InvalidDataException;
 import mr.limpios.smart_divide_backend.domain.exceptions.ResourceNotFoundException;
+import mr.limpios.smart_divide_backend.domain.models.ExpenseGroupBalance;
 import mr.limpios.smart_divide_backend.domain.models.Group;
 import mr.limpios.smart_divide_backend.domain.models.Payment;
 import mr.limpios.smart_divide_backend.domain.models.User;
@@ -33,6 +39,7 @@ public class PaymentService {
   private final PaymentRepository paymentRepository;
   private final UserRepository userRepository;
   private final GroupRepository groupRepository;
+  private final ExpenseGroupBalanceRepository balanceRepository;
   private final ApplicationEventPublisher eventPublisher;
 
   public List<PaymentDetailDTO> getPaymentsByGroup(String groupId) {
@@ -43,11 +50,11 @@ public class PaymentService {
 
   @Transactional
   public void createPayment(String userId, String groupId, CreatePaymentDTO createPaymentDTO) {
-
     Group group = groupRepository.getGroupById(groupId);
     if (Objects.isNull(group)) {
       throw new ResourceNotFoundException(GROUP_NOT_FOUND);
     }
+
     PaymentValidator.validate(createPaymentDTO, userId, group);
 
     User fromUser = userRepository.getUserbyId(createPaymentDTO.fromUserId());
@@ -59,17 +66,29 @@ public class PaymentService {
       throw new ResourceNotFoundException(USER_NOT_FOUND);
     }
 
+    Optional<ExpenseGroupBalance> existingBalance = balanceRepository.findByCreditorAndDebtorAndGroup(
+        fromUser.id(),
+        toUser.id(),
+        groupId);
+    if (existingBalance.isEmpty()) {
+      throw new InvalidDataException(NO_EXISTING_DEBTS_FOR_USER_PAIR);
+    }
+    ExpenseGroupBalance balance = existingBalance.get();
+
+    if (createPaymentDTO.amount().compareTo(balance.amount()) > 0) {
+      throw new InvalidDataException(PAYMENT_AMOUNT_EXCEEDS_DEBT);
+    }
+
     Payment payment = new Payment(
         null,
         fromUser,
         toUser,
         createPaymentDTO.amount(),
         group,
-        LocalDateTime.now()
-        );
+        LocalDateTime.now());
     Payment savedPayment = paymentRepository.savePayment(payment);
 
-    eventPublisher.publishEvent(new PaymentCreatedEvent(savedPayment));
+    eventPublisher.publishEvent(new PaymentCreatedEvent(savedPayment, balance));
   }
 
   private PaymentDetailDTO buildPaymentDetailDTO(Payment payment) {
